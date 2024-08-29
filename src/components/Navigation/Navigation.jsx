4f52d33279dab238f225d63503db493a0480fd8c
InnaIvBoiko <inna_boiko@libero.it>
2024-08-29 19:26:48 +0200

    Home page

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,24 @@
+import { Link } from 'react-router-dom';
 import css from './Navigation.module.css';
 
 export default function Navigation() {
     return (
-        <p>
-        Navigation
-        </p>
+        <nav className={css.container}>
+            <Link to={'/'}>
+                <img src="/public/Logo.png" alt="TravelTrucks" />
+            </Link>
+            <ul className={css.navMenu}>
+                <li>
+                    <Link to={'/'} className={css.navMenuItem}>
+                    Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to={'/catalog'} className={css.navMenuItem}>
+                    Catalog
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
